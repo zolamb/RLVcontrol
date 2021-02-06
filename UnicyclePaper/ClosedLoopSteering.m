acc95aa39b8e84ef7054931cf7278ca808790585
Zachary Lamb <zolamb@ucsc.edu>
2021-02-05 20:50:05 -0800

    Worked on lyapunov simulation

--- UnicyclePaper/ClosedLoopSteering.m
+++ UnicyclePaper/ClosedLoopSteering.m
@@ -1,7 +1,27 @@
-y0 = [sqrt(2) -pi/4 -pi];
-[t, y] = ode45(@(t,y)odeFunction2(y), [0 250], y0);
+clear;
+clc;
+% y0 = [sqrt(2) -pi/4 -pi];
+y0 = [-1 1 3*pi/4];
+dt = 0.01;
+
+u = 0;
+w = 0;
+
+yrec = [];
+
+for i=1:10000
+    [t, y] = ode45(@(t,y)odeFunction3(y, u, w), [0 dt], y0);
+    e = sqrt(y(end,1)^2 + y(end,2)^2);
+    theta = atan(y(end,2)/y(end,1));
+    alpha = theta - y(end,3);
+    u = 3*e*cos(alpha);
+    w = 6*alpha + 3*cos(alpha)*sin(alpha)*(alpha+1*theta)/alpha;
+    yrec = [yrec, y'];
+    y0 = y(end,:)';
+end
+
 % polarplot(y(:,3), y(:,1))
-plot(y(:,1).*cos(y(:,3)), y(:,1).*sin(y(:,3)))
+plot(yrec(:,1), yrec(:,2))
 
 % Simulate using eq.1
 % calculate new u,w with the new x,y from simulating eq.1
---
+++ UnicyclePaper/odeFunction3.m
@@ -0,0 +1,5 @@
+function dx = odeFunction3(x, u, w)
+    dx(1,1) = u*cos(x(3));
+    dx(2,1) = u*sin(x(3));
+    dx(3,1) = w;
+end
