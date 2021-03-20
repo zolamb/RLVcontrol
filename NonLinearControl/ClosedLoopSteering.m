8af98ad26601570bbca00b96a5efa95fcf2dc089
Zachary Lamb <zolamb@ucsc.edu>
2021-03-19 19:43:59 -0700

    Updated code and made LQR for vel,angVel

--- NonLinearControl/ClosedLoopSteering.m
+++ NonLinearControl/ClosedLoopSteering.m
@@ -2,6 +2,91 @@
 clear;
 clc;
 
+
+
+
+
+
+%% Initialize Vehicle Constants
+% RLV Physical Constants
+w = 3.7;            % width of rocket (m)
+L = 47.7;           % length of rocket (m)
+bL = 15.0;          % distance from center of rocket to center of mass (m)
+m = 250000.0;       % mass of rocket (kg)
+g = 9.81;           % acceleration due to gravity (m/s^2)
+Fw = m*g;           % weight of rocket (N)
+I = 0.5*m*(w/2)^2;  % inertia for a cylinder (1/2*m*r^2) (kg*m^2)
+
+%% Create System of ODE's
+% State variables: x1 x2 x3
+%       x1 -> vel
+%       x2 -> angular vel
+%       x3 -> body angle
+syms x1 x2 x3
+
+% Control variables: u1 u2 Ft psi
+%       u1 -> F1 + F2
+%       u2 -> F1 - F2
+%       Ft -> Thrust
+%       psi -> Thrust angle
+syms u1 u2 Ft psi
+
+% State Space
+ddotX = -(u1)/m*sin(x3) - (Ft/m)*sin(x3 + psi);
+ddotY = (u1)/m*cos(x3) + (Ft/m)*cos(x3 + psi) - Fw/m;
+ddotTheta = (w/(2*I))*(u2) - (Ft/I)*(L/2 - bL)*sin(psi);
+F = [sqrt(ddotX^2 + ddotY^2);
+     ddotTheta;
+     x2 
+     ];
+
+%% Create State Space Model
+% Creating list of state and control variables
+stateVars = [x1 x2 x3];
+controlVars = [u1 u2 Ft psi];
+
+% Take partial derivatives of F with respect to states
+A = jacobian(F, stateVars);
+
+% Take partial derivatives of F with respect to control inputs
+B = jacobian(F, controlVars);
+
+% Linearize about the trim state 'p'
+p = [0 0 0 0 0 0 0];
+v = [x1 x2 x3 u1 u2 Ft psi];
+A = double(subs(A,v,p));
+B = double(subs(B,v,p));
+C = [1 0 0;
+     0 1 0;
+     0 0 1];
+D = [0 0 0 0;
+     0 0 0 0;
+     0 0 0 0];
+
+%% LQR Controller Design
+% Determine controllability
+rank(ctrb(A,B)) % This should equal the number of states - 6
+
+% LQR Control
+Q = [100 0 0;         % vel
+     0 1e7 0;         % angular vel
+     0 0 1e7];              % angle pos.
+R = [1 0 0 0;               % u1 = f1+f2
+     0 1 0 0;               % u2 = f1-f2
+     0 0 1 0;               % Ft
+     0 0 0 1e9];            % Psi 
+K = lqr(A, B, Q, R);
+
+
+
+
+
+
+
+
+
+
+
 % Target parking pose:
 xP=5; yP=-5; thetaP=0; % we will use it in formulas for e and alpha
 
@@ -25,31 +110,30 @@ urec = [];
 wrec = [];
 trec=[];
 for i=1:500
-    % Compute e, alpha, and theta
-    % phi - robot heading, theta - heading of the parking pose
+    %%%% Compute e, alpha, and theta %%%%
     e=sqrt((xP-x)^2+(yP-y)^2); %distance between x,y and xP=0,yP=0
-    
     theta=atan2(yP-y,xP-x)-thetaP; % ThetaP is acting as an offset because the paper specifies equations where theta->0
-%     theta=atan2(yP-y,xP-x);
     alpha=theta-(phi-thetaP);
-%     alpha=theta-phi;
     alpha=atan2(sin(alpha),cos(alpha));
-
-    % Update controls
+    
+    
+    %%%%% Update Controls %%%%
     u = gamma*e*cos(alpha);
     if alpha <= 1e-50
       % lim alpha->0 (cos(alpha)*sin(alpha)/alpha) = 1
       w = k*alpha + gamma*cos(alpha)*sin(alpha)+...
-          gamma*h*theta;      
+          gamma*h*theta;
     else
       w = k*alpha + gamma*cos(alpha)*sin(alpha)*(alpha+h*theta)/alpha;
     end
     
+    
+    %%%% Solve Cartesian ODE %%%%
     % Update initial conditions
     y2Init=[x;y;phi];
     
     % Solve cartesian ODE for small timestep
-    [t, y2] = ode45(@(t,y2)odeFunction(t, y2, u, w), [0 dt], y2Init);
+    [t, y2] = ode45(@(t,y2)odeFunction2(y2, u, w), [0 dt], y2Init);
     
     % Record results
     yrec = [yrec, y2'];
--- NonLinearControl/odeFunction.m
+++ NonLinearControl/odeFunction.m
@@ -1,5 +1,17 @@
-function dx = odeFunction(t, x, u, w) % first parameter must be to be t
-    dx(1,1) = u*cos(x(3));
-    dx(2,1) = u*sin(x(3));
-    dx(3,1) = w;
+function dx = odeFunction(x, w, L, bL, m, Fw, I, u) % first parameter must be to be t
+    % Control inputs
+    F1=u(1);
+    F2=u(2);
+    Ft=u(3);
+    Psi=u(4);
+
+    % Newtonian set of ODEs
+    ddotX = -(F1+F2)/m*sin(x(3)) - (Ft/m)*sin(x(3) + Psi);
+    ddotY = (F1+F2)/m*cos(x(3)) + (Ft/m)*cos(x(3) + Psi) - Fw/m;
+    ddotTheta = (w/(2*I))*(F1-F2) - (Ft/I)*(L/2 - bL)*sin(Psi);
+
+    % Solve ODEs
+    dx(1,1) = sqrt(ddotX^2 + ddotY^2);
+    dx(2,1) = ddotTheta;
+    dx(3,1) = x(2); % Solving for theta
 end
